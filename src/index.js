1e18838ec29bacfc6c825729c1c97c2b8ced802b
Max Cabrera <cabrera.max@gmail.com>
2019-02-24 12:57:06 -0600

    persisting app state into local storage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import throttle from 'lodash/throttle'
+
 import App from './App'
 import store from './store'
 import * as serviceWorker from './serviceWorker'
+import { saveState } from "./utils/local-storage";
+
+store.subscribe(throttle(() => {
+  saveState({
+    user: store.getState().user,
+    repos: store.getState().repos,
+    errors: store.getState().errors,
+  })
+},1000))
 
 ReactDOM.render(
   <Provider store={store}>
